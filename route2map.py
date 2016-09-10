6f0e28ddaf6f459fe4b27b08db30f39329fcd319
Boying Xu <xuboying@gmail.com>
2016-09-10 23:19:08 +0800

    format with autopep8

--- route2map.py
+++ route2map.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from __future__ import print_function
 import sys
 import re
@@ -8,6 +8,7 @@ import struct
 import yaml
 import pygmaps
 
+
 def Open(filepath):
     if sys.platform.startswith('darwin'):
         subprocess.call(('open', filepath))
@@ -15,27 +16,33 @@ def Open(filepath):
         os.startfile(filepath)
     elif os.name == 'posix':
         subprocess.call(('xdg-open', filepath))
+
+
 def IPToNum(ip):
-    r = struct.unpack(">I", struct.pack("BBBB", * map( lambda a : int(a), ip.split("."))))
+    r = struct.unpack(">I", struct.pack(
+        "BBBB", * map(lambda a: int(a), ip.split("."))))
     return r[0]
+
+
 def GetAddress(ip):
     global index
-    #print(struct.pack("bbbb", * map( lambda a : int(a), ip.split("."))))
+    # print(struct.pack("bbbb", * map( lambda a : int(a), ip.split("."))))
     SEGLENGTH = 12
-    U         = os.path.getsize(index)
-    L         = 0
-    ip        = IPToNum(ip)
+    U = os.path.getsize(index)
+    L = 0
+    ip = IPToNum(ip)
     if IPToNum("192.168.0.0") <= ip <= IPToNum("192.168.255.255"):
         return "Local"
     with open(index, 'rb') as idx:
         while True:
-            M = int((int((U / SEGLENGTH - L / SEGLENGTH) / 2) + L / SEGLENGTH) * SEGLENGTH)
+            M = int((int((U / SEGLENGTH - L / SEGLENGTH) / 2) +
+                     L / SEGLENGTH) * SEGLENGTH)
             idx.seek(M)
             x = idx.read(4)
             n = struct.unpack(">I", x)
             n = n[0]
-            #print("%s  --->  " % n, end = "")
-            #print(struct.unpack("BBBB", x))
+            # print("%s  --->  " % n, end = "")
+            # print(struct.unpack("BBBB", x))
             if ip < n:
                 U = M
             else:
@@ -51,6 +58,8 @@ def GetAddress(ip):
                     r = r.decode('utf8')
                     r = re.sub(r"\n", r"", r)
                     return r
+
+
 def GetCoordinate(geo, addr):
     if addr == "Local":
         return None, None
@@ -59,34 +68,40 @@ def GetCoordinate(geo, addr):
         if addr[1] in geo[addr[0]].keys():
             if addr[2] in geo[addr[0]][addr[1]].keys():
                 coordinate = geo[addr[0]][addr[1]][addr[2]]
-                addr1      = ",".join(addr)
+                addr1 = ",".join(addr)
             else:
-                coordinate = geo[addr[0]][addr[1]][list(geo[addr[0]][addr[1]].keys())[2]]
-                addr1      = ",".join([addr[0], addr[1], list(geo[addr[0]][addr[1]].keys())[2]])
+                coordinate = geo[addr[0]][addr[1]][
+                    list(geo[addr[0]][addr[1]].keys())[2]]
+                addr1 = ",".join([addr[0], addr[1], list(
+                    geo[addr[0]][addr[1]].keys())[2]])
                 addr1 = "~" + addr1
         else:
-            coordinate = geo[addr[0]] [list(geo[addr[0]].keys())[0]] [list(geo[addr[0]][list(geo[addr[0]].keys())[0]].keys())[0]]
-            addr1      = ",".join([addr[0], list(geo[addr[0]].keys())[0], list(geo[addr[0]][list(geo[addr[0]].keys())[0]].keys())[0]])
-            addr1      = "~~" + addr1
-    return addr1, coordinate;
+            coordinate = geo[addr[0]][list(geo[addr[0]].keys())[0]][list(
+                geo[addr[0]][list(geo[addr[0]].keys())[0]].keys())[0]]
+            addr1 = ",".join([addr[0], list(geo[addr[0]].keys())[0], list(
+                geo[addr[0]][list(geo[addr[0]].keys())[0]].keys())[0]])
+            addr1 = "~~" + addr1
+    return addr1, coordinate
+
+
 def unique(seq):
-    seen     = set()
+    seen = set()
     seen_add = seen.add
     return[x for x in seq if not(x in seen or seen_add(x))]
 if __name__ == '__main__':
     index = "index"
     with open("geo.yml", "r") as f:
-        y   = f.read()
+        y = f.read()
         geo = yaml.load(y)
-    #p           = Popen('traceroute %s' % " ".join(sys.argv[1 : ]), stdout = PIPE, shell = True)
+    # p           = Popen('traceroute %s' % " ".join(sys.argv[1 : ]), stdout = PIPE, shell = True)
     coordinates = []
     while True:
         line = sys.stdin.readline()
         if not line:
             break
-        #line = p.stdout.readline()
+        # line = p.stdout.readline()
         if re.match(r"traceroute", line):
-            print(line, end = "")
+            print(line, end="")
             continue
         M = re.search(r"^(\s*(\d+)?\s+).*?\((\d+\.\d+\.\d+\.\d+)\)", line)
         if M:
@@ -98,18 +113,20 @@ if __name__ == '__main__':
                 addr.pop(0)
                 addr1, coordinate = GetCoordinate(geo, addr)
                 coordinates.append(coordinate)
-                print("%s%-15s%s %s %s" % (M.group(1), M.group(3), Addr, addr1, coordinate))
+                print("%s%-15s%s %s %s" %
+                      (M.group(1), M.group(3), Addr, addr1, coordinate))
             else:
                 print("%s%-15s%s" % (M.group(1), M.group(3), Addr))
         else:
-            print(line, end = "")
+            print(line, end="")
     coordinates = unique(coordinates)
-    coordinates = list(map( lambda x : list(map( lambda y : float(y), x.split(","))), coordinates))
+    coordinates = list(
+        map(lambda x: list(map(lambda y: float(y), x.split(","))), coordinates))
     if len(coordinates) == 0:
         sys.exit(0)
     mymap = pygmaps.maps(coordinates[0][0], coordinates[0][1], 2)
     mymap.addpoint(coordinates[0][0], coordinates[0][1], "#FF0000")
-    mymap.addpoint(coordinates[ - 1][0], coordinates[ - 1][1], "#0000FF")
+    mymap.addpoint(coordinates[- 1][0], coordinates[- 1][1], "#0000FF")
     for i in range(1, len(coordinates) - 1):
         mymap.addpoint(coordinates[i][0], coordinates[i][1], "#FFFF00")
     mymap.addpath(coordinates, "#FF0000")
@@ -118,4 +135,4 @@ if __name__ == '__main__':
     url = os.path.abspath(fn)
     url = "file://" + url
     Open(url)
-    #webbrowser.open_new_tab(url)
+    # webbrowser.open_new_tab(url)
